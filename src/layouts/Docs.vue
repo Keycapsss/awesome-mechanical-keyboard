27ce28474a2552c96a144c82b6f81e945a1f0dfb
BenRoe <mail@benjaminroesner.com>
2019-09-14 12:37:35 +0200

    Add test form

--- src/layouts/Docs.vue
+++ src/layouts/Docs.vue
@@ -77,6 +77,41 @@
             <span>Edit this page on GitHub</span>
           </a>
         </p>
+        
+        
+        <form 
+          name="contact"
+          method="post"
+          v-on:submit.prevent="handleSubmit"
+          action="/success/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
+          <div class="sender-info">
+            <div>
+              <label for="name" class="label" >Your name</label>
+              <input type="text" name="name" v-model="formData.name" />
+            </div>
+            <div>
+              <label for="email">Your email</label>
+              <input type="email" name="email" v-model="formData.email" />
+            </div>
+          </div>
+
+          <div class="message-wrapper">
+            <label for="message">Message</label>
+            <textarea name="message" v-model="formData.message"></textarea>
+          </div>
+
+          <button type="submit">Submit form</button>
+        </form>
+        
       </div>
   </Layout>
 </template>
@@ -85,10 +120,28 @@
 export default {
   data () {
     return {
-      showDropdownMenu: false
+      showDropdownMenu: false,
+      formData: {},
     }
   },
   methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/success'))
+      .catch(error => alert(error))
+    }
   },
   mounted: function () {
     const that = this
---
+++ src/pages/success.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto text-center py-16">
+      <h2 class="text-4xl font-bold">Submit success</h2>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Submit success'
+  }
+}
+</script>
+
