46e26aa978aee99fe1c94157e17c03f10cee2eb0
BenRoe <mail@benjaminroesner.com>
2021-10-16 13:08:58 +0200

    build: fix build error
    - update required node version to 16
    - update all npm packages to latest
    - fix quotation style in gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,78 +5,75 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Keebfol.io',
-  siteDescription: 'Curated list of mechanical keyboard resources.',
-  siteUrl: 'https://keebfol.io',
+  siteName: "Keebfol.io",
+  siteDescription: "Curated list of mechanical keyboard resources.",
+  siteUrl: "https://keebfol.io",
   chainWebpack(config) {
-    config.mode('development')
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
+    config.mode("development");
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   },
-  plugins: [{
-      use: 'gridsome-plugin-tailwindcss',
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
       options: {
-        // tailwindConfig: './some/file/js',
-        // purgeConfig: {},
-        // presetEnvConfig: {},
-        shouldPurge: false, // https://www.purgecss.com/with-postcss
-        shouldImport: true, // https://github.com/postcss/postcss-import
-        shouldTimeTravel: true, // https://github.com/csstools/postcss-preset-env
-      }
+        tailwindConfig: "./tailwind.config.js",
+        presetEnvConfig: {},
+        shouldImport: false,
+        shouldTimeTravel: false,
+      },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        index: ['README'],
-        path: '**/*.md',
-        baseDir: './docs',
-        typeName: 'DocPage',
+        index: ["README"],
+        path: "**/*.md",
+        baseDir: "./docs",
+        typeName: "DocPage",
         remark: {
           autolinkHeadings: {
             content: {
-              type: 'text',
-              value: '#',
-            }
-          }
-        }
-      }
+              type: "text",
+              value: "#",
+            },
+          },
+        },
+      },
     },
     {
-      use: 'gridsome-plugin-rss',
+      use: "gridsome-plugin-rss",
       options: {
-        contentTypeName: 'CommitMessages',
+        contentTypeName: "CommitMessages",
         feedOptions: {
-          title: 'Keebfol.io - A Mechanical Keyboard Wiki',
-          feed_url: 'https://keebfol.io/rss.xml',
-          site_url: 'https://keebfol.io',
+          title: "Keebfol.io - A Mechanical Keyboard Wiki",
+          feed_url: "https://keebfol.io/rss.xml",
+          site_url: "https://keebfol.io",
         },
-        feedItemOptions: node => ({
+        feedItemOptions: (node) => ({
           guid: node.id,
           date: node.date,
           title: node.message,
           description: node.body,
           author: node.author,
-          url: 'https://keebfol.io/',
+          url: "https://keebfol.io/",
         }),
         output: {
-          dir: './static/',
-          name: 'rss.xml',
+          dir: "./static/",
+          name: "rss.xml",
         },
         latest: true,
-      }
+      },
     },
     {
-      use: 'gridsome-plugin-modal',
+      use: "gridsome-plugin-modal",
     },
   ],
   transformers: {
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-    }
-  }
-}
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+    },
+  },
+};
