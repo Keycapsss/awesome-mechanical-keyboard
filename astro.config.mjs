852df5287437b52bb89b92b98307dd145a216ea5
Ben Roe <BenRoe@users.noreply.github.com>
2025-12-23 11:06:01 +0100

    perf: improve SEO and site performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
   site: `https://keebfolio.netlify.app`,
   trailingSlash: 'never',
+  prefetch: true,
 });
